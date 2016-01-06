73fb10d6d67e8d6e86400ae30d1a3073e0c64692
curink <curink@gmail.com>
2016-01-07 00:29:00 +0700

    Create importer.php

---
+++ importer.php
@@ -0,0 +1,27 @@
+<?php
+class Importer {
+
+	var $database = null;
+	var $connection = null;
+	var $compress = null;
+	var $utf8 = null;
+
+	var $importFilename = null;
+
+	function Importer($db=null, $connection=null, $filepath='dump.sql', $compress=false) {
+		$this->connection = $connection;
+		$this->compress = $compress;
+		$this->importFilename = $filepath;
+
+		$this->utf8 = true;
+
+		return $this->setDatabase($db);
+	}
+
+	function setDatabase($db){
+		$this->database = $db;
+		if ( !@mysql_select_db($this->database) )
+			return false;
+		return true;
+  	}
+	
