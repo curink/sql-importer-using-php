4c7c715d7584aa6951c32930e13be6129f3db27d
curink <curink@gmail.com>
2016-01-07 00:30:16 +0700

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,20 @@
+<?php
+
+@include_once('Importer.php');
+
+$dbhost = "localhost";
+$dbuser = "user";
+$dbpass = "password";
+$dbname = "database";
+
+$path      = "backup/";
+$filename  = $path."dump.sql";
+$compress  = false;
+
+$connection = @mysql_connect($dbhost,$dbuser,$dbpass);
+$dump = new Importer($dbname,$connection,$filename,$compress);
+
+$dump->utf8 = true;
+
+$dump->doImport();
+?>
